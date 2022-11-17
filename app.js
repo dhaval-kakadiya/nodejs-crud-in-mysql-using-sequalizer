9d62247c4bab0e8000d5570ff8a21128addd63f0
Dhaval Kakadiya <113237680+dhaval-kakadiya@users.noreply.github.com>
2022-11-17 03:17:33 +0000

    feat: add comment

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = process.env.PORT || 3000;
 require("./config/db.config");
 const routes = require("./routes/index")
 
+// for parse body data
 app.use(express.json({limit:'50mb'}))
 app.use(express.urlencoded({limit:'50mb',extended:false}))
 
